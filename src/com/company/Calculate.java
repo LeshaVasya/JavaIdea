f1ba192df0bb79c816f18763d764be7856fd60ec
LeshaVasya <leshavasya@gmail.com>
2016-02-01 00:20:09 +0300

    Initial one

---
+++ src/com/company/Calculate.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Calculate {
+
+    public static void main(String[] args) {
+        System.out.println("Calculate...");
+        int a = 2;
+        int b = 3;
+        int c = Math.
+    }
+}
