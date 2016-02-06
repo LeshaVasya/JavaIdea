66cc568a9222a41b25c4113d986692e5e9525a34
LeshaVasya <leshavasya@gmail.com>
2016-02-06 17:34:17 +0300

    Add PetClinic Maven project

---
+++ PetClinic/src/main/java/Cat.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Vasyamfx on 06.02.2016.
+ */
+public class Cat extends Animal implements Pet {
+    public  static void  main(String args[]){
+        
+    }
+}
---
+++ PetClinic/src/main/java/Dog.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Vasyamfx on 06.02.2016.
+ */
+public class Dog extends  Animal implements  Pet {
+}
