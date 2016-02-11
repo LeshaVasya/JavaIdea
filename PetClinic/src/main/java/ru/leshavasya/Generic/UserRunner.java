1b9a57d306f846d399cf11f9459fa86060d2b14b
LeshaVasya <leshavasya@gmail.com>
2016-02-11 11:19:21 +0300

    HashMap added

--- PetClinic/src/main/java/ru/leshavasya/Generic/UserRunner.java
+++ PetClinic/src/main/java/ru/leshavasya/Generic/UserRunner.java
@@ -25,6 +25,12 @@ public class UserRunner {
         userHashSet.add(new User("3","Third"));
         userHashSet.add(new User("1","First"));
 
+        Map<String, User> userHashMap = new HashMap<String, User>();
+        userHashMap.put("1", new User("1","First"));
+        userHashMap.put("2", new User("2","Second"));
+        userHashMap.put("3", new User("3","Third"));
+        userHashMap.put("1", new User("1","First"));
+
 
         //ArrayList includes internal iterator
         System.out.println("=================ArrayList=================");
@@ -43,11 +49,19 @@ public class UserRunner {
         System.out.println("======================================================");
 
         System.out.println("====================HashSet================");
-        //ArrayList includes internal iterator
         for (User user:userHashSet) {
             System.out.println(user);
             System.out.println("User "+user.getName()+" HashCode = "+user.hashCode());
         }
+        System.out.println("======================================================");
+
+        System.out.println("====================HashMap================");
+        //HashMap doesn't include internal iterator. Should use EntrySet()
+        for (Map.Entry<String, User> user:userHashMap.entrySet()) {
+            System.out.println(user);
+            System.out.println("User "+user.getValue().getName()+" HashCode = "+user.getValue().hashCode());
+        }
+
         System.out.println("======================================================");
     }
 }
