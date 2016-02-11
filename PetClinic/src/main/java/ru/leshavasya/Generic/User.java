4269c6d6f182c258e948f899d15b2ce96d52487c
LeshaVasya <leshavasya@gmail.com>
2016-02-11 10:33:03 +0300

    Add Generic ArrayList

---
+++ PetClinic/src/main/java/ru/leshavasya/Generic/User.java
@@ -0,0 +1,22 @@
+package ru.leshavasya.Generic;
+
+/**
+ * Created by Vasyamfx on 11.02.2016.
+ */
+public class User {
+    private String id;
+    private String name;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ PetClinic/src/main/java/ru/leshavasya/Generic/UserRunner.java
@@ -0,0 +1,20 @@
+package ru.leshavasya.Generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Vasyamfx on 11.02.2016.
+ */
+public class UserRunner {
+    public static void main(String[] args){
+        List<User> userArrayList = new ArrayList<User>();
+        userArrayList.add(new User("1","First"));
+        userArrayList.add(new User("2","Second"));
+        userArrayList.add(new User("3","Third"));
+
+        for (User user:userArrayList) {
+            System.out.println(user);
+        }
+    }
+}
