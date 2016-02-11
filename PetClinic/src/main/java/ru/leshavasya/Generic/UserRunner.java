ceeab55afb09e74e92bd5e329b1017808c280d27
LeshaVasya <leshavasya@gmail.com>
2016-02-11 11:03:23 +0300

    HashSet Added

--- PetClinic/src/main/java/ru/leshavasya/Generic/UserRunner.java
+++ PetClinic/src/main/java/ru/leshavasya/Generic/UserRunner.java
@@ -1,7 +1,6 @@
 package ru.leshavasya.Generic;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Vasyamfx on 11.02.2016.
@@ -14,10 +13,41 @@ public class UserRunner {
         userArrayList.add(new User("3","Third"));
         userArrayList.add(new User("1","First"));
 
+        List<User> userLinkedList = new LinkedList<User>();
+        userLinkedList.add(new User("1","First"));
+        userLinkedList.add(new User("2","Second"));
+        userLinkedList.add(new User("3","Third"));
+        userLinkedList.add(new User("1","First"));
+
+        Set<User> userHashSet = new HashSet<User>();
+        userHashSet.add(new User("1","First"));
+        userHashSet.add(new User("2","Second"));
+        userHashSet.add(new User("3","Third"));
+        userHashSet.add(new User("1","First"));
+
+
         //ArrayList includes internal iterator
+        System.out.println("=================ArrayList=================");
         for (User user:userArrayList) {
             System.out.println(user);
             System.out.println("User "+user.getName()+" HashCode = "+user.hashCode());
         }
+        System.out.println("======================================================");
+
+        System.out.println("====================LinkedList================");
+        //ArrayList includes internal iterator
+        for (User user:userLinkedList) {
+            System.out.println(user);
+            System.out.println("User "+user.getName()+" HashCode = "+user.hashCode());
+        }
+        System.out.println("======================================================");
+
+        System.out.println("====================HashSet================");
+        //ArrayList includes internal iterator
+        for (User user:userHashSet) {
+            System.out.println(user);
+            System.out.println("User "+user.getName()+" HashCode = "+user.hashCode());
+        }
+        System.out.println("======================================================");
     }
 }
