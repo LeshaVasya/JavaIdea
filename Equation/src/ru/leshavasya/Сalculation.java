d85402bc3dc00d4a5cf4f0eef1e67cdbe69f13dd
v-vasia-gri@ae.com <v-vasia-gri@ae.com>
2016-05-27 14:43:37 -0700

    Switch added

--- Equation/src/ru/leshavasya/Сalculation.java
+++ Equation/src/ru/leshavasya/Сalculation.java
@@ -8,6 +8,7 @@ public class Сalculation {
     private final double a;
     private final double b;
     private final double c;
+    private final double zero = 0.0;
 
     public Сalculation(double a, double b, double c) {
         this.a = a;
@@ -15,21 +16,27 @@ public class Сalculation {
         this.c = c;
     }
 
-   private Double getDiscriminant (double a, double b, double c){
+   private Double getDiscriminant (){
         return  b*b - 4*a*c;
-    }
-
-   private Boolean checkDiscriminant (double discriminant){
-       return discriminant >= 0;
    }
 
-    public void Calculate(){
-        double discriminant = getDiscriminant(a,b,c);
-        if(!checkDiscriminant(discriminant)){
-            System.out.println("The equation couldn't be solved because discriminant is less than zero: "+discriminant);
-            System.out.println("a = "+a);
-            System.out.println("b = "+b);
-            System.out.println("c = "+c);
+   private int checkDiscriminant (double discriminant){
+       return  Double.compare(discriminant, zero);
+   }
+
+    public void calculate(){
+        double discriminant = getDiscriminant();
+        switch (checkDiscriminant(discriminant)){
+            case 1:
+                break;
+            case 0:
+                break;
+            case -1:
+                System.out.println("The equation couldn't be solved because discriminant is less than zero: "+discriminant);
+                System.out.println("a = "+a);
+                System.out.println("b = "+b);
+                System.out.println("c = "+c);
+                break;
         }
     }
 }
