b7364c2701ee5ffb15a13d8ee8ee7d22659302de
v-vasia-gri@ae.com <v-vasia-gri@ae.com>
2016-05-27 13:15:11 -0700

    New application template created

---
+++ Equation/src/ru/leshavasya/Main.java
@@ -0,0 +1,8 @@
+package ru.leshavasya;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Equation/src/ru/leshavasya/Сalculation.java
@@ -0,0 +1,8 @@
+package ru.leshavasya;
+
+/**
+ * @author avasiliev
+ *         Created: 2016-05-27.
+ */
+public class Сalculation {
+}
