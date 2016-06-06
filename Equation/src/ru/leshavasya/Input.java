073e19221a41934b6f345c8c7aa2ea341ae26a3a
v-vasia-gri@ae.com <v-vasia-gri@ae.com>
2016-06-05 21:02:07 -0700

    Input is done

--- Equation/src/ru/leshavasya/Input.java
+++ Equation/src/ru/leshavasya/Input.java
@@ -8,55 +8,29 @@ import java.util.Scanner;
  *         Created: 2016-05-27.
  */
 public class Input {
-    private static double getValueFromConsole(String variable) {
-        Scanner scanner = new Scanner(System.in);
-        Double val = null;
+    private static double getValueFromConsole(String input) {
+        Scanner reader = new Scanner(System.in);
+        Double variable = null;
         do {
-            System.out.println(String.format("Please enter the '%s' value:", variable));
+            System.out.println(String.format("Please enter the '%s' double  value:", input));
             try {
-                  val = scanner.nextDouble();
-                 } catch (InputMismatchException e) {
-                    System.out.println(String.format("​Incorrect value. Please enter correct '%s' value:", variable));
-                       scanner.next();
-                     }
-            catch (NumberFormatException nfe){
-
+                variable = reader.nextDouble();
             }
-               } while (val == null);
-            return val;
-
-
-    }
-
-    Scanner reader = new Scanner(System.in);
-    try{
-        Calculator calc = new Calculator();
-        String exit = "no";
-        while (!exit.equals("yes")){
-            System.out.println("Enter first arg: ");
-            String first = reader.next();
-            System.out.println("Enter second arg: ");
-            String second = reader.next();
-            calc.add(Integer.valueOf(first), Integer.valueOf(second));
-            System.out.println("The addiction result is: "+calc.getResult());
-            try {
-                calc.div(Integer.valueOf(first), Integer.valueOf(second));
-            } catch (UserException ue) {
-                System.out.println(ue.getMessage());
-                ue.printStackTrace();
+            catch (InputMismatchException ime) {
+                System.out.println(String.format("​Incorrect '%s' value. Possibly not double. Please try again:", input));
+                System.out.print(ime.getMessage());
+                reader.next();
             }
-            System.out.println("The division result is: "+calc.getResult());
-            calc.cleanResult();
-            System.out.println("Exit? (yes/no)");
-            exit = reader.next();
+            catch (NumberFormatException nfe) {
+                System.out.println(String.format("​Incorrect '%s' value. Possibly using comma instead of dot. Please try again:", input));
+                System.out.print(nfe.getMessage());
+                reader.next();
+            }
+            finally{
+                reader.close();
+            }
         }
-    }
-    //Where is catch?
-    catch(NumberFormatException nfe){
-        System.out.println("Args should be Integer numbers!");
-        System.out.println(nfe.getMessage());
-    }
-    finally{
-        reader.close();
+        while (variable == null);
+        return variable;
     }
 }
