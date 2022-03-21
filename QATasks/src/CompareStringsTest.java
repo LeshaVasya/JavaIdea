0c1c027356b59636067a2ad74ed56d1bb3cca9ff
Alexey Vasiliev <leshavasya@gmail.com>
2022-03-22 00:00:30 +0100

    Add 2 more String objects to compare

--- QATasks/src/CompareStringsTest.java
+++ QATasks/src/CompareStringsTest.java
@@ -2,8 +2,12 @@ public class CompareStringsTest {
     public static void main(String[] args) {
         String a = "Car";
         String b = "Car";
+        String c = new String("Car");
+        String d = new String("Car");
         System.out.println(a == b);
+        System.out.println(c == d);
+        System.out.println(a == c);
         System.out.println(a.equals(b));
-        System.out.println(b.equals(a));
+        System.out.println(c.equals(d));
     }
 }
